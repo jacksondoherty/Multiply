69695b94c281e1bcd22ecf84e01473a24d64fe1a
Jackson Doherty <jgdoherty23@gmail.com>
2017-01-02 19:26:00 -0500

    complete, online game

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -11,9 +11,17 @@ public class Health : NetworkBehaviour {
 	[SyncVar(hook = "OnChangeHealth")]
 	public int currentHealth = maxHealth;
 	public RectTransform healthBar;
+	public bool destroyOnDeath;
 
-	public void TakeDamage(int amount)
-	{
+	private NetworkStartPosition[] spawnPoints;
+
+	void Start() {
+		if (isLocalPlayer) {
+			spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+		}
+	}
+
+	public void TakeDamage(int amount) {
 		if (!isServer) {
 			return;
 		}
@@ -21,8 +29,12 @@ public class Health : NetworkBehaviour {
 		currentHealth -= amount;
 		if (currentHealth <= 0)
 		{
-			currentHealth = maxHealth;
-			RpcRespawn();
+			if (destroyOnDeath) {
+				Destroy (gameObject);
+			} else {
+				currentHealth = maxHealth;
+				RpcRespawn();
+			}
 		}
 	}
 
@@ -33,8 +45,17 @@ public class Health : NetworkBehaviour {
 	[ClientRpc]
 	void RpcRespawn() {
 		if (isLocalPlayer) {
-			// transform back to original position
-			transform.position = Vector3.zero;
+			// Set the spawn point to origin as a default value
+			Vector3 spawnPoint = Vector3.zero;
+
+			// If there is a spawn point array and the array is not empty, pick a spawn point at random
+			if (spawnPoints != null && spawnPoints.Length > 0)
+			{
+				spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+			}
+
+			// Set the player’s position to the chosen spawn point
+			transform.position = spawnPoint;
 		}
 	}
 
