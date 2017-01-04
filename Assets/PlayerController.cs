d3c1761a164e644b691ce2a5ffcdaa021dab0f36
Jackson Doherty <jgdoherty23@gmail.com>
2017-01-04 16:22:52 -0500

    mouse look

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,54 +5,77 @@ using System.Collections;
 
 public class PlayerController : NetworkBehaviour {
 
+	public float movementSpeed;
+	public float turnSpeed;
+	public float bulletSpeed;
 	public GameObject bulletPrefab;
 	public Transform bulletSpawn;
 
+	private Vector3 movement;
+	private float turn;
+
 	public override void OnStartLocalPlayer() {
+		// change character color
 		GetComponent<MeshRenderer>().material.color = Color.blue;
 
-		if (isLocalPlayer) {
-			// set position of camera and set as child of this
-			Camera.main.transform.position = transform.position
-											- transform.forward * 10
-											+ transform.up * 3;
-			Camera.main.transform.rotation = transform.rotation;
-			Camera.main.transform.parent = transform;
-		}
+		// setup camera
+		Camera.main.transform.position = transform.position
+			- transform.forward * 10
+			+ transform.up * 3;
+		Camera.main.transform.rotation = transform.rotation;
+		Camera.main.transform.parent = transform;
 
+		Cursor.visible = false;
 	}
 
 	void Update () {
-		// keep first -> so only local player executes code below
-		if (!isLocalPlayer) {
-			return;
-		}
+		if (isLocalPlayer) {
+			GunControl ();
+			MovementControl ();
+			TurnControl ();
 
-		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (Input.GetKey (KeyCode.C)) {
+				Cursor.visible = !Cursor.visible;
+			}
+		}
+	}
+		
+	void GunControl() {
+		if (Input.GetMouseButtonDown(0)) {
 			CmdFire ();
 		}
+	}
 
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+	void MovementControl() {
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		float vertical = Input.GetAxisRaw ("Vertical");
 
-		transform.Rotate(0, x, 0);
-		transform.Translate(0, 0, z);
+		movement.Set(horizontal, 0, vertical);
+		movement = movement.normalized * Time.deltaTime * movementSpeed;
+		transform.Translate(movement);
+	}
+
+	void TurnControl() {
+		if (Input.mousePosition.x < Screen.width * .01) {
+			turn = -turnSpeed;
+		} else if (Input.mousePosition.x > Screen.width * .99) {
+			turn = turnSpeed;
+		} else {
+			turn = Input.GetAxis ("Mouse X") * turnSpeed;
+		}
+		transform.Rotate (0, turn, 0);
 	}
 
 	[Command]
 	void CmdFire() {
-		// Create the Bullet from the Bullet Prefab
 		var bullet = (GameObject)Instantiate (
 			bulletPrefab,
 			bulletSpawn.position,
 			bulletSpawn.rotation);
-
-		// Add velocity to the bullet
-		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
-
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
 		NetworkServer.Spawn (bullet);
 
-		// Destroy the bullet after 2 seconds
-		Destroy(bullet, 2.0f);
+		// destroy bullet after 10 seconds
+		Destroy(bullet, 10.0f);
 	}
 }
