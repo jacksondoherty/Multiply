9cca51cce83868eb6857ccf5e88356190f6a1e92
Jackson Doherty <jgdoherty23@gmail.com>
2017-01-29 22:18:06 -0600

    add online/offline scenes

---
+++ Assets/MyHud.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(NetworkManager))]
+
+public class MyHud : MonoBehaviour {
+	public NetworkManager manager;
+
+	void Awake()
+	{
+		manager = GetComponent<NetworkManager>();
+	}
+
+	void OnGUI() {
+		
+	}
+}
